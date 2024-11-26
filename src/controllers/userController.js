8e4aa20c8da7611b6a422e0c9b96caf3fdf7b2fb
Andrey Bejarano <andreybejarano@gmail.com>
2024-11-26 20:55:07 -0300

    feat: transform html pages to EJS pages

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,9 @@
-const path = require('path');
-
 const controller = {
     login: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/login.html'));
+        res.render('login');
     },
     register: (req, res) => {
-        res.sendFile(path.join(__dirname, '../views/register.html'));
+        res.render('register');
     }
 }
 
