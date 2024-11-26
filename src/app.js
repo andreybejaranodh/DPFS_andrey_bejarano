efff6bcc63a2c0274a85b0bbed82cc2fde4f894a
Andrey Bejarano <andreybejarano@gmail.com>
2024-11-26 20:52:39 -0300

    chore: install and set up ejs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', mainRoutes);
